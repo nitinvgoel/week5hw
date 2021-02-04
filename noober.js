67385debf721947591b0eda0437bdc49636c9721
Nitin Goel <nitin.goel@kellogg.northwestern.edu>
2021-02-04 11:43:37 -0600

    final push hw5

--- noober.js
+++ noober.js
@@ -11,18 +11,15 @@ function levelOfService(ride) {
   }
   return levelOfService
 }
-
 function renderRides(ridesArray) {
   for (let i = 0; i < ridesArray.length; i++) {
     let ride = ridesArray[i]
-
     document.querySelector('.rides').insertAdjacentHTML('beforeend', `
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
         <i class="fas fa-car-side"></i>
         <span>${levelOfService(ride)}</span>
       </h1>
     `)
-
     let borderClass
     let backgroundClass
     if (levelOfService(ride) == 'Noober Purple') {
@@ -32,10 +29,8 @@ function renderRides(ridesArray) {
       borderClass = 'border-gray-900'
       backgroundClass = 'bg-gray-600'
     }
-
     for (let i = 0; i < ride.length; i++) {
       let leg = ride[i]
-
       document.querySelector('.rides').insertAdjacentHTML('beforeend', `
         <div class="border-4 ${borderClass} p-4 my-4 text-left">
           <div class="flex">
@@ -66,8 +61,76 @@ function renderRides(ridesArray) {
     }
   }
 }
-
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', async function() {
   // YOUR CODE
+  let response = await fetch("https://kiei451.com/api/rides.json")
+  let ride = await response.json()
+  let AllRides = document.querySelector("#all-filter")
+  AllRides.addEventListener("click", function(event){
+    event.preventDefault()
+    let ClearHTML = document.querySelector(".rides")
+    ClearHTML.innerHTML = ""
+    renderRides(ride)
+  })
+  let ridesDisplayed = []
+  let NooberPurple = document.querySelector("#noober-purple-filter")
+  NooberPurple.addEventListener("click", function(event){
+    event.preventDefault()
+    alert('You have selected Noober Purple')
+    console.log("You have selected Noober Purple")
+    let ridesDisplayed = []
+    for (i=0; i<ride.length; i++){
+      if (levelOfService(ride[i]) == 'Noober Purple'){  
+        ridesDisplayed.push(ride[i])
+      }
+    }
+    let ClearHTML = document.querySelector(".rides")
+    ClearHTML.innerHTML = ""
+    renderRides(ridesDisplayed)
+  })
+  let NooberPool = document.querySelector("#noober-pool-filter")
+  NooberPool.addEventListener("click", function(event){
+    event.preventDefault()
+    alert('You have selected Noober Pool')
+    console.log("You have selected Noober Pool")
+    let ridesDisplayed = []
+    for (i=0; i<ride.length; i++){
+      if (levelOfService(ride[i]) == 'Noober Pool'){  
+        ridesDisplayed.push(ride[i])
+      }
+    }
+    let ClearHTML = document.querySelector(".rides")
+    ClearHTML.innerHTML = ""
+    renderRides(ridesDisplayed)
+  })
+  let NooberXL = document.querySelector("#noober-xl-filter")
+  NooberXL.addEventListener("click", function(event){
+    event.preventDefault()
+    alert('You have selected Noober XL')
+    console.log("You have selected Noober XL")
+    let ridesDisplayed = []
+    for (i=0; i<ride.length; i++){
+      if (levelOfService(ride[i]) == 'Noober XL'){  
+        ridesDisplayed.push(ride[i])
+      }
+    }
+    let ClearHTML = document.querySelector(".rides")
+    ClearHTML.innerHTML = ""
+    renderRides(ridesDisplayed)
+  })
+  let NooberX = document.querySelector("#noober-x-filter")
+  NooberX.addEventListener("click", function(event){
+    event.preventDefault()
+    alert('You have selected Noober X')
+    console.log("You have selected Noober X")
+    let ridesDisplayed = []
+    for (i=0; i<ride.length; i++){
+      if (levelOfService(ride[i]) == 'Noober X'){  
+        ridesDisplayed.push(ride[i])
+      }
+    }
+    let ClearHTML = document.querySelector(".rides")
+    ClearHTML.innerHTML = ""
+    renderRides(ridesDisplayed)
+  })
 })
-
